5d082ed0278425391e954f33bb883a31d518dc83
YxngXi1 <yang@xue.email>
2024-05-30 14:40:28 -0400

    index, yourinfo pages complete

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ def index():
     average_price = sum(prices) / len(prices)
     return render_template('index.html', prices=prices, average_price=average_price)
 
-@app.route('/about')
+@app.route('/yourinfo')
 def about():
-    return render_template('about.html')
+    return render_template('yourinfo.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
